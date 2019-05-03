ae6cdde2c5cab2f7c585935eea3609edf1bc1397
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-03 17:31:49 +0200

    Create Car.java

---
+++ src/main/java/com/zetcode/model/Car.java
@@ -0,0 +1,59 @@
+package com.zetcode.model;
+
+import java.util.Objects;
+
+public class Car {
+
+    private Long id;
+    private String name;
+    private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return price == car.price &&
+                Objects.equals(id, car.id) &&
+                Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Car{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        return sb.toString();
+    }
+}
